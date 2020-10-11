8085dbdb8bc3e0f6a5d389af4db8528e71ef1952
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2020-10-12 04:27:01 +0530

    refactor: Moved to specific spec file.

--- spec/ledger_sync/ledgers/xero/resources/version_spec.rb
+++ spec/ledger_sync/ledgers/xero/resources/version_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe LedgerSync::Ledgers::Xero do
   it 'has a version number' do
     expect(LedgerSync::Ledgers::Xero::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
