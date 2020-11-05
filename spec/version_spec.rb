c0255202fd83e1b2a2471a2730c90babbfe0cca7
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2020-11-05 13:48:34 +0530

    test(version): Changed test to check constant defined.

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -3,7 +3,5 @@
 require 'spec_helper'
 
 RSpec.describe LedgerSync::Xero do
-  it 'has a version number' do
-    expect(LedgerSync::Xero::VERSION).not_to be nil
-  end
+  it { should be_const_defined(:VERSION) }
 end
