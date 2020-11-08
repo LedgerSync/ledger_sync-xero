c402ac36ecbe946927dce7addcdb88af20f72820
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2020-11-08 17:38:50 +0530

    test(unit): Added Unit Tests for `Account`.

---
+++ spec/account/update_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+support :xero_shared_examples
+
+RSpec.describe LedgerSync::Xero::Account::Operations::Update, operation: true do
+  it_behaves_like 'a xero operation'
+end
+
