ae3083d06806fef5e85988590dbacc1d3cd84a17
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2020-12-22 22:42:57 +0530

    text(refactor): usage of serializer defined before (DRYing)

--- spec/unit/account/serializer_for_status_spec.rb
+++ spec/unit/account/serializer_for_status_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe LedgerSync::Xero::Account::StatusSerializer, unit: true do
 
   it "should not serialize any other key except status" do
     account = LedgerSync::Xero::Account.new Status: 'active'
-    expect(described_class.new.serialize(resource: account).keys).to include "Status", "AccountID"
+    expect(serializer.serialize(resource: account).keys).to include "Status", "AccountID"
 
   end
 end
