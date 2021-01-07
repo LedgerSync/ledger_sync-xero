1a8958ba6cef1a48ac23e06c2e3a59c54c2f734f
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2021-01-07 19:54:16 +0530

    test: take out variable

--- spec/unit/account/serializer_for_status_spec.rb
+++ spec/unit/account/serializer_for_status_spec.rb
@@ -4,9 +4,21 @@ require 'spec_helper'
 
 RSpec.describe LedgerSync::Xero::Account::SerializerForStatus, unit: true do
   let(:serializer) { described_class.new }
+  # TODO: DRY this with serializer ?
+  let(:account) do
+    LedgerSync::Xero::Account.new(
+      Status: 'active',
+      TaxType: 'input',
+      BankAccountNumber: 'test',
+      BankAccountType: 'bank',
+      Code: 'code',
+      CurrencyCode: 'USD',
+      Name: 'Name',
+      Type: 'bank'
+    )
+  end
 
   it 'should not serialize any other key except status' do
-    account = LedgerSync::Xero::Account.new Status: 'active', TaxType: 'input'
     expect(serializer.serialize(resource: account)).to eq({ 'AccountID' => nil, 'Status' => 'ACTIVE' })
   end
 end
