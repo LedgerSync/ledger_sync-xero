9e7c73f45fd339579e7639caa895a5b52c1a4ba0
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2021-01-07 18:25:41 +0530

    chore: remove todo

--- lib/ledger_sync/xero/account/operations/update_status.rb
+++ lib/ledger_sync/xero/account/operations/update_status.rb
@@ -7,7 +7,6 @@ module LedgerSync
 				class UpdateStatus < Xero::Operation::UpdatePost
 					class Contract < LedgerSync::Ledgers::Contract
 						params do
-							# TODO: Create new serializer. status_serializer, serializer_for_status
 							required(:ledger_id).filled(:string)
 							required(:external_id).filled(:string)
 							required(:Status).filled(:string)
