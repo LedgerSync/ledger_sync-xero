d008f714f3028e355e7028be0cf2a982bb37636b
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2021-03-02 16:53:44 +0530

    refactor: module based rspec structuring

--- spec/unit/account/serializer_spec.rb
+++ spec/unit/account/serializer_spec.rb
@@ -2,31 +2,37 @@
 
 require 'spec_helper'
 
-RSpec.describe LedgerSync::Xero::Account::Serializer, unit: true do
-  let(:serializer) { described_class.new }
-  let(:account) do
-    LedgerSync::Xero::Account.new(
-      Status: 'active',
-      TaxType: 'input',
-      BankAccountNumber: 'test',
-      BankAccountType: 'bank',
-      Code: 'code',
-      CurrencyCode: 'USD',
-      Name: 'Name',
-      Type: 'bank'
-    )
-  end
+module LedgerSync
+  module Xero
+    class Account
+      RSpec.describe Serializer, unit: true do
+        let(:serializer) { described_class.new }
+        let(:account) do
+          LedgerSync::Xero::Account.new(
+            Status: 'active',
+            TaxType: 'input',
+            BankAccountNumber: 'test',
+            BankAccountType: 'bank',
+            Code: 'code',
+            CurrencyCode: 'USD',
+            Name: 'Name',
+            Type: 'bank'
+          )
+        end
 
-  it 'should not serialize any other key except status' do
-    expect(serializer.serialize(resource: account)).to eq({
-                                                            'AccountID' => nil,
-                                                            'BankAccountNumber' => 'test',
-                                                            'Code' => 'code',
-                                                            'CurrencyCode' => 'USD',
-                                                            'Name' => 'Name',
-                                                            'Status' => nil,
-                                                            'Type' => 'BANK',
-                                                            'BankAccountType' => 'BANK'
-                                                          })
+        it 'should not serialize any other key except status' do
+          expect(serializer.serialize(resource: account)).to eq({
+                                                                  'AccountID' => nil,
+                                                                  'BankAccountNumber' => 'test',
+                                                                  'Code' => 'code',
+                                                                  'CurrencyCode' => 'USD',
+                                                                  'Name' => 'Name',
+                                                                  'Status' => nil,
+                                                                  'Type' => 'BANK',
+                                                                  'BankAccountType' => 'BANK'
+                                                                })
+        end
+      end
+    end
   end
 end
