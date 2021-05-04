ec253563ea4e9f6e2ae48f3b1c156a3828cea7ad
Ananthu Sudhikumar <45742784+amouxkin@users.noreply.github.com>
2021-05-04 20:28:29 +0530

    Refactor/module based rspec structure (#8)
    * Chore/ci setup (#6)
    
    * config(ci): github actions workflow
    
    * config(ci): support for all tags
    
    * refactor: change ci name
    
    * style: new line
    
    * config(ci): coveralls setup
    
    * chore: remove environment (#7)
    
    * config(ci): remove redundant travis config
    
    * refactor: module based rspec structuring
    
    * refactor: module based rspec structuring
    
    * refactor: module based rspec structuring
    
    * refactor: module based rspec structuring
    
    * refactor: module based rspec structuring
    chore: remove todo
    
    * refactor: module based rspec structuring
    
    * refactor: module based rspec structuring
    
    * refactor: simplified
    
    * refactor: module based rspec structuring
    
    * style: rubocop
    
    Co-authored-by: Ryan Jackson <ryanwjackson@gmail.com>

--- spec/unit/account/operations/update_spec.rb
+++ spec/unit/account/operations/update_spec.rb
@@ -2,34 +2,42 @@
 
 require 'spec_helper'
 
-RSpec.describe LedgerSync::Xero::Account::Operations::Update, unit: true do
-  # TODO: Shared example for this ?
-  describe '.inferred_resource_class' do
-    it { expect(described_class.inferred_resource_class).to eq(LedgerSync::Xero::Account) }
-  end
+module LedgerSync
+  module Xero
+    class Account
+      module Operations
+        RSpec.describe Update, unit: true do
+          # TODO: Shared example for this ?
+          describe '.inferred_resource_class' do
+            it { expect(described_class.inferred_resource_class).to eq(Account) }
+          end
 
-  describe '.request_body_as_array?' do
-    it { expect(described_class.request_body_as_array?).to be_falsey }
-  end
+          describe '.request_body_as_array?' do
+            it { expect(described_class.request_body_as_array?).to be_falsey }
+          end
 
-  describe '.ledger_id_in_path?' do
-    it { expect(described_class.ledger_id_in_path?).to be_truthy }
-  end
+          describe '.ledger_id_in_path?' do
+            it { expect(described_class.ledger_id_in_path?).to be_truthy }
+          end
 
-  describe '.ledger_resource_path' do
-    it { expect(described_class.ledger_resource_path(ledger_id: 'account_id')).to eq('Accounts/account_id') }
-  end
+          describe '.ledger_resource_path' do
+            it { expect(described_class.ledger_resource_path(ledger_id: 'account_id')).to eq('Accounts/account_id') }
+          end
 
-  describe '.request_method' do
-    it { expect(described_class.request_method).to eq(:post) }
-  end
+          describe '.request_method' do
+            it { expect(described_class.request_method).to eq(:post) }
+          end
 
-  describe '.request_body' do
-    let(:body) { { 'asdf' => 'blah' } }
-    it { expect(described_class.request_body(body: body)).to eq(body) }
-  end
+          describe '.request_body' do
+            let(:body) { { 'asdf' => 'blah' } }
+            it { expect(described_class.request_body(body: body)).to eq(body) }
+          end
 
-  describe '.response_body_as_array?' do
-    it { expect(described_class.response_body_as_array?).to be_truthy }
+          describe '.response_body_as_array?' do
+            it { expect(described_class.response_body_as_array?).to be_truthy }
+          end
+        end
+      end
+    end
   end
 end
--- spec/unit/contact/operations/find_spec.rb
+++ spec/unit/contact/operations/find_spec.rb
@@ -2,32 +2,37 @@
 
 require 'spec_helper'
 
-RSpec.describe LedgerSync::Xero::Contact::Operations::Find, unit: true do
-  describe '.inferred_resource_class' do
-    it { expect(described_class.inferred_resource_class).to eq(LedgerSync::Xero::Contact) }
-  end
+module LedgerSync
+  module Xero
+    class Contact
+      module Operations
+        RSpec.describe Find, unit: true do
+          describe '.inferred_resource_class' do
+            it { expect(described_class.inferred_resource_class).to eq(Contact) }
+          end
 
-  describe '.request_body_as_array?' do
-    it { expect(described_class.request_body_as_array?).to be_falsey }
-  end
+          describe '.request_body_as_array?' do
+            it { expect(described_class.request_body_as_array?).to be_falsey }
+          end
 
-  describe '.ledger_id_in_path?' do
-    it { expect(described_class.ledger_id_in_path?).to be_truthy }
-  end
+          describe '.ledger_id_in_path?' do
+            it { expect(described_class.ledger_id_in_path?).to be_truthy }
+          end
 
-  describe '.ledger_resource_path' do
-    it {
-      # TODO: 'contact_ud' in a variable ?
-      expect(described_class.ledger_resource_path(ledger_id: 'contact_id')).to eq('Contacts/contact_id')
-    }
-  end
+          describe '.ledger_resource_path' do
+            it { expect(described_class.ledger_resource_path(ledger_id: 'contact_id')).to eq('Contacts/contact_id') }
+          end
 
-  describe '.request_body' do
-    let(:body) { { 'asdf' => 'blah' } }
-    it { expect(described_class.request_body(body: body)).to eq(body) }
-  end
+          describe '.request_body' do
+            let(:body) { { 'asdf' => 'blah' } }
+            it { expect(described_class.request_body(body: body)).to eq(body) }
+          end
 
-  describe '.response_body_as_array?' do
-    it { expect(described_class.response_body_as_array?).to be_truthy }
+          describe '.response_body_as_array?' do
+            it { expect(described_class.response_body_as_array?).to be_truthy }
+          end
+        end
+      end
+    end
   end
 end
